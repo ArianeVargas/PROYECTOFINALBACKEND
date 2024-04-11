13e29cbd7db56afa83fb0bc39841c5986f6169a1
eri323 <jomaldonado480@gmail.com>
2024-04-11 16:23:40 -0500

    erick

--- models/DistribucionLoteDependencia.js
+++ models/DistribucionLoteDependencia.js
@@ -4,7 +4,7 @@ const disLoteDependenciaSchema = new mongoose.Schema({
     Codigo: { type: String, index:"text", require:true, unique:true},
     PresupuestoAsignado: { type:Number, require:true},
     PresupuestoDisponible: {type:Number, require:true},
-    DistribucionDependencia_id:{type:mongoose.Schema.Types.ObjectId,ref:'DisDependencia', require:true},
+    Dependencia_id:{type:mongoose.Schema.Types.ObjectId,ref:'DisDependencia', require:true},
     DistribucionContratoLote_id:{type:mongoose.Schema.Types.ObjectId,ref:'DistribucionContratoLote', require:true},
     createAT : {type:Date,default: Date.now },
     Estado:{type:Boolean, default:1}
